1507e3061c6e32a2a6f27742f4ff5d28ed3e87e0
aleksey.usachev <aleksey.usachev@meistersoft.ru>
2019-11-10 15:58:16 +0300

    done

--- 5-solid-change/index.js
+++ 5-solid-change/index.js
@@ -1,117 +1,196 @@
-// Новое требование: сравнивать несколько разных кубиков
+// Новое требование: хотим сравнивать несколько кубиков на одном графике
 var numberOfTries = 1000;
-var morales = new Array(18).fill(null).map(function(_, i) {
-  return i + 6;
-}); // 6-24
+var morales = new Array(18).fill(null).map(function (_, i) { return i + 6; }); // 6-24
+function rand(max) {
+    return Math.floor(Math.random() * max + 1);
+}
 function getRoller(die) {
-  return function() {
-    return die[Math.floor(Math.random() * 6)];
-  };
+    return function () { return die[Math.floor(Math.random() * 6)]; };
+}
+function getRollers(dice) {
+    return dice.map(getRoller);
 }
 function getRollToFail(totalMorale, roll) {
-  var rollToFail = 0;
-  var morale = totalMorale;
-  while (morale > 0) {
-    morale -= roll();
-    rollToFail++;
-  }
-  return rollToFail;
+    var rollToFail = 0;
+    var morale = totalMorale;
+    while (morale > 0) {
+        morale -= roll();
+        rollToFail++;
+    }
+    return rollToFail;
 }
 function getRollsToFail(totalMorale, roll) {
-  var rollsToFail = [];
-  for (var tryNumber = 0; tryNumber < numberOfTries; tryNumber++) {
-    rollsToFail.push(getRollToFail(totalMorale, roll));
-  }
-  return rollsToFail;
+    var rollsToFail = [];
+    for (var tryNumber = 0; tryNumber < numberOfTries; tryNumber++) {
+        rollsToFail.push(getRollToFail(totalMorale, roll));
+    }
+    return rollsToFail;
 }
 function getGroupedRolls(rollsToFail) {
-  return rollsToFail.reduce(function(acc, rollsToFail, index) {
-    var key = String(rollsToFail);
-    if (acc[rollsToFail]) {
-      acc[key]++;
-    } else {
-      acc[key] = 1;
-    }
-    return acc;
-  }, {});
+    return rollsToFail.reduce(function (acc, rollsToFail, index) {
+        var key = String(rollsToFail);
+        if (acc[rollsToFail]) {
+            acc[key]++;
+        }
+        else {
+            acc[key] = 1;
+        }
+        return acc;
+    }, {});
+}
+function getGroupedArrayRolls(groupedResults) {
+    var min = Math.max.apply(Math, morales), max = 1;
+    groupedResults.forEach(function (groupedResult) {
+        var keys = Object.keys(groupedResult).map(Number);
+        var minKey = Math.min.apply(Math, keys);
+        var maxKey = Math.max.apply(Math, keys);
+        if (minKey < min) {
+            min = minKey;
+        }
+        if (maxKey > max) {
+            max = maxKey;
+        }
+    });
+    var normalizedResults = [];
+    groupedResults.forEach(function (groupedResults, index) {
+        normalizedResults.push({});
+        for (var i = min; i <= max; i++) {
+            normalizedResults[index][i] = groupedResults[i] || 0;
+        }
+    });
+    return normalizedResults;
 }
 function getLabels(rollsToFailGrouped) {
-  return Object.keys(rollsToFailGrouped).sort(function(key) {
-    return Number(key);
-  });
+    return Object.keys(rollsToFailGrouped).sort(function (key) { return Number(key); });
 }
-function getChartDatas(roll) {
-  return morales.map(function(totalMorale) {
-    var rollsToFail = getRollsToFail(totalMorale, roll);
-    var rollsToFailGrouped = getGroupedRolls(rollsToFail);
-    var labels = getLabels(rollsToFailGrouped);
-    var chartData = {
-      type: "bar",
-      data: {
-        datasets: [
-          {
-            data: labels.map(function(label) {
-              return rollsToFailGrouped[label];
-            }),
-            label: "flat",
-            backgroundColor: labels.map(function() {
-              return "rgba(255, 99, 132, 0.2)";
-            }),
-            borderColor: labels.map(function() {
-              return "rgba(255,99,132,1)";
-            })
-          }
-        ],
-        labels: labels
-      }
-    };
-    return chartData;
-  });
-}
-function main(faces) {
-  if (faces === void 0) {
-    faces = [1, 1, 2, 2, 3, 3];
-  }
-  function drawCharts(chartDatas) {
-    var container = document.createElement("div");
-    container.id = "container";
-    var Chart = window["Chart"];
-    chartDatas.forEach(function(chartData, index) {
-      var header = document.createElement("h2");
-      header.innerText = "Мораль: " + morales[index];
-      var canvas = document.createElement("canvas");
-      container.appendChild(header);
-      container.appendChild(canvas);
-      var chart = new Chart(canvas.getContext("2d"), chartData);
+function getChartDatas(rolls) {
+    var colors = rolls.map(function (_) { return "rgba(" + rand(255) + ", " + rand(99) + ", " + rand(132) + ", 0.2)"; });
+    return morales.map(function (totalMorale) {
+        var rollsToFailGrouped = getGroupedArrayRolls(rolls.map(function (roll) { return getGroupedRolls(getRollsToFail(totalMorale, roll)); }));
+        var labels = getLabels(rollsToFailGrouped[0]);
+        var chartData = {
+            type: "bar",
+            data: {
+                datasets: rollsToFailGrouped.map(function (rolls, index) { return ({
+                    data: labels.map(function (label) { return rolls[label]; }),
+                    label: "" + index,
+                    backgroundColor: labels.map(function () { return colors[index]; }),
+                    borderColor: labels.map(function () { return "rgba(255,99,132,1)"; })
+                }); }),
+                labels: labels
+            }
+        };
+        return chartData;
     });
-    var oldContainer = document.getElementById("container");
-    if (oldContainer) {
-      document.body.removeChild(oldContainer);
-    }
-    document.body.appendChild(container);
-  }
-  function onInputChange(event, faceIndex) {
-    var value = Number(event.target.value);
-    faces[faceIndex] = value;
-    onParamsChange(faces);
-  }
-  function onParamsChange(die) {
-    var roller = getRoller(die);
-    var chartDatas = getChartDatas(roller);
-    drawCharts(chartDatas);
-  }
-  function bindToInputs(onInputChange) {
-    var _loop_1 = function(face) {
-      var input = document.getElementById("face" + face);
-      input.addEventListener("input", function(event) {
-        return onInputChange(event, face);
-      });
+}
+function createHeader(totalMorale) {
+    var heading = document.createElement("h2");
+    heading.innerText = "\u041C\u043E\u0440\u0430\u043B\u044C: " + totalMorale;
+    return heading;
+}
+function getContainer() {
+    return document.getElementById("container");
+}
+function createCanvas() {
+    return document.createElement("canvas");
+}
+function createInput(index, value) {
+    if (value === void 0) { value = 1; }
+    var input = document.createElement("input");
+    input.className = "face" + index;
+    input.type = "number";
+    input.value = String(value);
+    input.min = String(1);
+    return input;
+}
+function createInputGroup(faces) {
+    var group = document.createElement("div");
+    faces.forEach(function (face, index) {
+        group.appendChild(createInput(index, face));
+    });
+    group.className = "inputGroup";
+    return group;
+}
+function bindToInputGroup(inputGroup, onInputChange) {
+    var _loop_1 = function (face) {
+        var input = inputGroup.querySelector(".face" + face);
+        input.addEventListener("input", function (event) { return onInputChange(event, face); });
     };
     for (var face = 0; face <= 5; face++) {
-      _loop_1(face);
+        _loop_1(face);
     }
-  }
-  bindToInputs(onInputChange);
-  onParamsChange(faces);
+}
+function bindToInputGroups(inputGroups, onInputGroupChange) {
+    inputGroups.forEach(function (inputGroup, groupIndex) {
+        bindToInputGroup(inputGroup, function (event, faceIndex) {
+            return onInputGroupChange(event, groupIndex, faceIndex);
+        });
+    });
+}
+function createContainer(chartDatas) {
+    var Chart = window["Chart"];
+    var container = document.createElement("div");
+    container.id = "container";
+    chartDatas.forEach(function (chartData, index) {
+        var header = createHeader(morales[index]);
+        var canvas = createCanvas();
+        container.appendChild(header);
+        container.appendChild(canvas);
+        var chart = new Chart(canvas.getContext("2d"), chartData);
+    });
+    return container;
+}
+function createInputGroups(faces) {
+    return faces.map(createInputGroup);
+}
+function createAddButton(onClick) {
+    var button = document.createElement("button");
+    button.addEventListener("click", onClick);
+    button.innerText = "добавить кубик";
+    return button;
+}
+function renderFaces(faces) {
+    function renderContainer(chartDatas) {
+        var container = createContainer(chartDatas);
+        var oldContainer = getContainer();
+        if (oldContainer) {
+            document.body.removeChild(oldContainer);
+        }
+        document.body.appendChild(container);
+    }
+    function renderInputGroups(inputGrups) {
+        var oldGroups = document.querySelectorAll(".inputGroup");
+        if (oldGroups) {
+            oldGroups.forEach(function (inputGroup) {
+                return document.body.removeChild(inputGroup);
+            });
+        }
+        inputGrups.forEach(function (g) { return document.body.appendChild(g); });
+    }
+    function onInputGroupChange(event, groupIndex, faceIndex) {
+        var value = Number(event.target.value);
+        faces[groupIndex][faceIndex] = value;
+        onParamsChange(faces);
+    }
+    function onParamsChange(dice) {
+        renderContainer(getChartDatas(getRollers(dice)));
+    }
+    var inputGroups = createInputGroups(faces);
+    renderInputGroups(inputGroups);
+    bindToInputGroups(inputGroups, onInputGroupChange);
+    onParamsChange(faces);
+}
+function main() {
+    var defaultDie = [1, 1, 2, 2, 3, 3];
+    var dice = [[1, 1, 2, 2, 3, 3], [1, 2, 2, 2, 2, 3]];
+    function onAddClick() {
+        dice.push(defaultDie);
+        renderFaces(dice);
+    }
+    function renderAddButton() {
+        document.body.appendChild(createAddButton(onAddClick));
+    }
+    renderAddButton();
+    renderFaces(dice);
 }
 main();
--- 5-solid-change/index.ts
+++ 5-solid-change/index.ts
@@ -1,4 +1,4 @@
-// Новое требование: сравнивать несколько разных кубиков
+// Новое требование: хотим сравнивать несколько кубиков на одном графике
 
 interface ChartData {
   type: "bar";
@@ -21,8 +21,16 @@ type Roll = () => number;
 const numberOfTries = 1000;
 const morales: number[] = new Array(18).fill(null).map((_, i) => i + 6); // 6-24
 
+function rand(max: number): number {
+  return Math.floor(Math.random() * max + 1);
+}
+
 function getRoller(die: Faces): Roll {
   return () => die[Math.floor(Math.random() * 6)];
+}
+
+function getRollers(dice: Faces[]): Roll[] {
+  return dice.map(getRoller);
 }
 
 function getRollToFail(totalMorale: number, roll: Roll): number {
@@ -55,26 +63,57 @@ function getGroupedRolls(rollsToFail: number[]): GroupedResults {
   }, {});
 }
 
+function getGroupedArrayRolls(
+  groupedResults: GroupedResults[]
+): GroupedResults[] {
+  let min = Math.max(...morales),
+    max = 1;
+  groupedResults.forEach((groupedResult: GroupedResults) => {
+    const keys: number[] = Object.keys(groupedResult).map(Number);
+    const minKey: number = Math.min(...keys);
+    const maxKey: number = Math.max(...keys);
+    if (minKey < min) {
+      min = minKey;
+    }
+    if (maxKey > max) {
+      max = maxKey;
+    }
+  });
+  const normalizedResults: GroupedResults[] = [];
+
+  groupedResults.forEach((groupedResults: GroupedResults, index: number) => {
+    normalizedResults.push({});
+    for (let i = min; i <= max; i++) {
+      normalizedResults[index][i] = groupedResults[i] || 0;
+    }
+  });
+  return normalizedResults;
+}
+
 function getLabels(rollsToFailGrouped: GroupedResults): string[] {
   return Object.keys(rollsToFailGrouped).sort((key: string) => Number(key));
 }
 
-function getChartDatas(roll: Roll): ChartData[] {
+function getChartDatas(rolls: Roll[]): ChartData[] {
+  const colors: string[] = rolls.map(
+    _ => `rgba(${rand(255)}, ${rand(99)}, ${rand(132)}, 0.2)`
+  );
   return morales.map((totalMorale: number) => {
-    const rollsToFail = getRollsToFail(totalMorale, roll);
-    const rollsToFailGrouped: GroupedResults = getGroupedRolls(rollsToFail);
-    const labels: string[] = getLabels(rollsToFailGrouped);
+    const rollsToFailGrouped: GroupedResults[] = getGroupedArrayRolls(
+      rolls.map(roll => getGroupedRolls(getRollsToFail(totalMorale, roll)))
+    );
+    const labels: string[] = getLabels(rollsToFailGrouped[0]);
     const chartData: ChartData = {
       type: "bar",
       data: {
-        datasets: [
-          {
-            data: labels.map((label: string) => rollsToFailGrouped[label]), // Array<number>
-            label: "flat",
-            backgroundColor: labels.map(() => "rgba(255, 99, 132, 0.2)"),
-            borderColor: labels.map(() => "rgba(255,99,132,1)")
-          }
-        ],
+        datasets: rollsToFailGrouped.map(
+          (rolls: GroupedResults, index: number) => ({
+            data: labels.map((label: string) => rolls[label]),
+            label: `${index}`,
+            backgroundColor: labels.map(() => colors[index]),
+            borderColor: labels.map(() => `rgba(255,99,132,1)`)
+          })
+        ),
         labels
       }
     };
@@ -82,48 +121,139 @@ function getChartDatas(roll: Roll): ChartData[] {
   });
 }
 
-function getSetOfInputs() {}
+function createHeader(totalMorale: number): HTMLHeadingElement {
+  const heading: HTMLHeadingElement = document.createElement("h2");
+  heading.innerText = `Мораль: ${totalMorale}`;
+  return heading;
+}
 
-function main(faces: Faces[]) {
-  function drawCharts(chartDatas: ChartData[]): void {
-    const container: HTMLElement = document.createElement("div");
-    container.id = "container";
-    const Chart = window["Chart"];
-    chartDatas.forEach((chartData: ChartData, index: number) => {
-      const header: HTMLHeadingElement = document.createElement("h2");
-      header.innerText = "Мораль: " + morales[index];
-      const canvas: HTMLCanvasElement = document.createElement("canvas");
-      container.appendChild(header);
-      container.appendChild(canvas);
-      const chart = new Chart(canvas.getContext("2d"), chartData);
-    });
+function getContainer(): HTMLElement {
+  return document.getElementById("container");
+}
 
-    const oldContainer: HTMLElement = document.getElementById("container");
+function createCanvas(): HTMLCanvasElement {
+  return document.createElement("canvas");
+}
+
+function createInput(index: number, value: number = 1): HTMLInputElement {
+  const input: HTMLInputElement = document.createElement("input");
+  input.className = `face${index}`;
+  input.type = "number";
+  input.value = String(value);
+  input.min = String(1);
+  return input;
+}
+
+function createInputGroup(faces: Faces): HTMLDivElement {
+  const group: HTMLDivElement = document.createElement("div");
+  faces.forEach((face: number, index: number) => {
+    group.appendChild(createInput(index, face));
+  });
+  group.className = "inputGroup";
+  return group;
+}
+
+function bindToInputGroup(
+  inputGroup: HTMLDivElement,
+  onInputChange: (event: Event, faceIndex: number) => void
+): void {
+  for (let face = 0; face <= 5; face++) {
+    const input: HTMLElement = inputGroup.querySelector(`.face${face}`);
+    input.addEventListener("input", event => onInputChange(event, face));
+  }
+}
+
+function bindToInputGroups(
+  inputGroups: HTMLDivElement[],
+  onInputGroupChange: (
+    event: Event,
+    groupIndex: number,
+    faceIndex: number
+  ) => void
+) {
+  inputGroups.forEach((inputGroup: HTMLDivElement, groupIndex: number) => {
+    bindToInputGroup(inputGroup, (event: Event, faceIndex: number) =>
+      onInputGroupChange(event, groupIndex, faceIndex)
+    );
+  });
+}
+
+function createContainer(chartDatas: ChartData[]): HTMLDivElement {
+  const Chart = window["Chart"];
+  const container: HTMLDivElement = document.createElement("div");
+  container.id = "container";
+  chartDatas.forEach((chartData: ChartData, index: number) => {
+    const header = createHeader(morales[index]);
+    const canvas: HTMLCanvasElement = createCanvas();
+    container.appendChild(header);
+    container.appendChild(canvas);
+    const chart = new Chart(canvas.getContext("2d"), chartData);
+  });
+  return container;
+}
+
+function createInputGroups(faces: Faces[]): HTMLDivElement[] {
+  return faces.map(createInputGroup);
+}
+
+function createAddButton(onClick: (event: Event) => void): HTMLButtonElement {
+  const button: HTMLButtonElement = document.createElement("button");
+  button.addEventListener("click", onClick);
+  button.innerText = "добавить кубик";
+  return button;
+}
+
+function renderFaces(faces: Faces[]): void {
+  function renderContainer(chartDatas: ChartData[]): void {
+    const container: HTMLDivElement = createContainer(chartDatas);
+    const oldContainer: HTMLElement = getContainer();
     if (oldContainer) {
       document.body.removeChild(oldContainer);
     }
     document.body.appendChild(container);
   }
-  function onInputChange(event: Event, faceIndex: number): void {
-    const value = Number((event.target as HTMLInputElement).value);
-    faces[faceIndex] = value;
-    onParamsChange(faces);
-  }
-  function onParamsChange(die: Faces): void {
-    const roller = getRoller(die);
-    const chartDatas: ChartData[] = getChartDatas(roller);
-    drawCharts(chartDatas);
-  }
-  function bindToInputs(
-    onInputChange: (event: Event, faceIndex: number) => void
-  ) {
-    for (let face = 0; face <= 5; face++) {
-      const input: HTMLElement = document.getElementById(`face${face}`);
-      input.addEventListener("input", event => onInputChange(event, face));
+  function renderInputGroups(inputGrups: HTMLDivElement[]): void {
+    const oldGroups: NodeListOf<HTMLDivElement> = document.querySelectorAll<
+      HTMLDivElement
+    >(".inputGroup");
+    if (oldGroups) {
+      oldGroups.forEach((inputGroup: HTMLDivElement) =>
+        document.body.removeChild(inputGroup)
+      );
     }
+    inputGrups.forEach(g => document.body.appendChild(g));
   }
 
-  bindToInputs(onInputChange);
+  function onInputGroupChange(
+    event: Event,
+    groupIndex: number,
+    faceIndex: number
+  ): void {
+    const value = Number((event.target as HTMLInputElement).value);
+    faces[groupIndex][faceIndex] = value;
+    onParamsChange(faces);
+  }
+  function onParamsChange(dice: Faces[]): void {
+    renderContainer(getChartDatas(getRollers(dice)));
+  }
+  const inputGroups: HTMLDivElement[] = createInputGroups(faces);
+  renderInputGroups(inputGroups);
+  bindToInputGroups(inputGroups, onInputGroupChange);
   onParamsChange(faces);
 }
-main([[1, 1, 2, 2, 3, 3]]);
+
+function main() {
+  const defaultDie: Faces = [1, 1, 2, 2, 3, 3];
+  const dice: Faces[] = [[1, 1, 2, 2, 3, 3], [1, 2, 2, 2, 2, 3]];
+  function onAddClick() {
+    dice.push(defaultDie);
+    renderFaces(dice);
+  }
+  function renderAddButton(): void {
+    document.body.appendChild(createAddButton(onAddClick));
+  }
+
+  renderAddButton();
+  renderFaces(dice);
+}
+main();
